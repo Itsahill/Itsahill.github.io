54dff960a1080d6d89eb6dd57c79c8cb2807945d
s-luqman <zrekker@gmail.com>
2025-10-19 17:24:42 -0400

    completed hobby site

--- itis3135/hobby/scripts/reveal.js
+++ itis3135/hobby/scripts/reveal.js
@@ -1,2 +1,8 @@
 const sections = document.querySelectorAll('.wrapper');
 const buttons = document.querySelectorAll('.btn');
+buttons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        sections.forEach(section => section.style.display = 'none');
+        sections[index].style.display = 'block';
+    });
+});
